8c60d588968eb1f50416b27aedd6d52069228505
Mark <mrkcarlos576@gmail.com>
2024-04-10 12:17:33 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      console.log(input.value);
+  
+      fetch("http://localhost:3000/movies")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          // LOG: (3) [{…}, {…}, {…}]
+        });
+        
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+    };
 
 document.addEventListener('DOMContentLoaded', init);
+
+const inputForm = document.querySelector("form");
+
+inputForm.addEventListener("submit", (event) => {});
+  
+  document.addEventListener("DOMContentLoaded", init);
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+  });
+
+  event.target.children[1].value;
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+  
+    console.log(input.value);
+  });
+
+
+
+  
